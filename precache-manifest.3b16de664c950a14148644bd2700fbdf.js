f10b5d98344884ccd34e96ba2bb60f20af33b33f
Koiraladeepesh <dipeshkoirala4@gmail.com>
2023-01-08 17:20:50 +0545

    Updates

---
+++ precache-manifest.3b16de664c950a14148644bd2700fbdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fbb199400c8795a74f9",
+    "url": "/text-utility/static/css/main.1b72c45a.chunk.css"
+  },
+  {
+    "revision": "3fbb199400c8795a74f9",
+    "url": "/text-utility/static/js/main.3fbb1994.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/text-utility/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/text-utility/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "326b7c3ed529ad54513e",
+    "url": "/text-utility/static/js/runtime~main.326b7c3e.js"
+  },
+  {
+    "revision": "5b0b75264430787d338040662d6258d9",
+    "url": "/text-utility/index.html"
+  }
+];
